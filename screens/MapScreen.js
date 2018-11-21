c56fa472792aef770971b6e4c73b2507941b70a7
Carlos Saavedra <carlos.saavedra.regalado@gmail.com>
2018-11-21 11:59:31 -0800

    Fix map screen and orientation styles

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import MapView from 'react-native-maps';
-import { ScreenOrientation } from 'expo';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { MapView, ScreenOrientation } from 'expo';
 import Header from '../containers/Header';
 
 export default class MapScreen extends Component {
@@ -11,14 +10,14 @@ export default class MapScreen extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            region: {
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            }
-        }
+
+        this.state = Dimensions.get('window');
+
+        this.handleOrientation = this.handleOrientation.bind(this);
+    }
+
+    componentWillMount() {
+        Dimensions.addEventListener('change', this.handleOrientation);
     }
 
     componentDidMount() {
@@ -27,20 +26,24 @@ export default class MapScreen extends Component {
 
     componentWillUnmount() {
         ScreenOrientation.allowAsync(ScreenOrientation.Orientation.PORTRAIT);
+        Dimensions.removeEventListener('change', this.handleOrientation);
     }
 
-    onRegionChange(region) {
-        this.setState({ region });
+    handleOrientation(dims) {
+        this.setState(dims.window);
     }
 
     render() {
+        const {width, height} = this.state;
+        let mapStyle = height > width ? styles.mapPortrait : styles.mapLandscape;
         return(
-            <View>
+            <View style={ styles.container }>
                 <Header title={ this.navigationOptions.title } />
                 <MapView
+                    style={ mapStyle }
                     initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
+                        latitude: 32.52375,
+                        longitude: -117.01466,
                         latitudeDelta: 0.0922,
                         longitudeDelta: 0.0421,
                     }}
@@ -49,3 +52,16 @@ export default class MapScreen extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    mapPortrait: {
+        flex: 8
+    },
+    mapLandscape: {
+        flex: 4
+    }
+});
+
